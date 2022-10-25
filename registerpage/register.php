026ed7bb1b3e4aac25989066fb010768d2aef494
Tejas Srinivas <109969195+tejas-srinivas@users.noreply.github.com>
2022-10-25 13:28:24 +0530

    Registration Page

---
+++ registerpage/register.php
@@ -0,0 +1,44 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
+        $DATABASE_HOST = 'localhost';
+        $DATABASE_USER = 'root';
+        $DATABASE_PASS = '';
+        $DATABASE_NAME = 'registration_form';
+
+        $con = mysqli_connect($DATABASE_HOST,$DATABASE_USER,$DATABASE_PASS,$DATABASE_NAME);
+    
+        if(mysqli_connect_error()) {
+            exit('Error connecting to the database'.mysqli_connect_error());
+        }
+
+        if(isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && isset($_POST['username']) && isset($_POST['password'])) {
+            $first_name = $_POST['first_name'];
+            $last_name = $_POST['last_name'];
+            $email = $_POST['email'];
+            $username = $_POST['username'];
+            $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
+
+            $select = "SELECT * from users WHERE username='$username'";
+            $result = mysqli_query($con,$select);
+            if($result) {
+                $num = mysqli_num_rows($result);
+                if($num>0) {
+                    echo "<script>alert('User already exists');</script>";
+                }
+                else {
+                    $stmt= $con->prepare('INSERT INTO users(first_name,last_name,email,username,password) VALUES (?,?,?,?,?)');   //end-to-end password protection
+                    $stmt->bind_param('sssss',$first_name,$last_name,$email,$username,$password);  //s=string ,i=integer
+                    $stmt->execute(); //executes the function
+                    if($stmt) {
+                        echo "<script>alert('Registration Sucessfull !');</script>";
+                        echo "<script>window.location.href='../loginpage/login.html'</script>";
+                    }
+                    else {
+                        echo 'Error Occured inserting into records';
+                    }
+                }
+            }
+        }    
+    }
+
+?>
