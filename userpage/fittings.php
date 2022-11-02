f67c06c29ba8f474cfa977dd4a96cb7d35cc1654
tejas-srinivas <stejas2002@gmail.com>
2022-11-02 19:39:55 +0530

    vechile bookings page

--- userpage/fittings.php
+++ userpage/fittings.php
@@ -16,7 +16,7 @@
     <link href="user_details.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="user_style.css" rel="stylesheet" type="text/css">
-    <title>VEHICAL ENQUIRY</title>
+    <title>Fittings</title>
   </head>
   <body>
     <nav class="designer-slider">
@@ -87,3 +87,11 @@
       </div>
     </div> 
   </nav>  
+  <section class="home">
+      <div class="text"><h1 style="color:#000"><b>Fittings</b></h1></div>
+      <div class="border"></div>
+      <br>
+      <br>
+  </section>
+  </body>
+</html>
