6e492c31f46fff79f16a5a36d5d1955899d25f80
tejas-srinivas <stejas2002@gmail.com>
2022-11-22 22:11:22 +0530

    changes in UI

--- userpage/updates_changes.php
+++ userpage/updates_changes.php
@@ -16,8 +16,6 @@ if (!isset($_SESSION['username'])) {
 
   <!-- CDN CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <!-- Bootstrap CSS -->
-
   <link href="user_style.css" rel="stylesheet" type="text/css">
   <title>VEHICAL ENQUIRY</title>
 </head>
