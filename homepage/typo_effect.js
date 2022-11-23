2f404dfa22d9007fb93757e97014aa66953648d8
tejas-srinivas <stejas2002@gmail.com>
2022-11-24 01:39:48 +0530

    Typo_effect

--- homepage/typo_effect.js
+++ homepage/typo_effect.js
@@ -1,11 +1,31 @@
-var i=0,text;
-text = "Wheels and Deals"
+// const typedTextSpan = document.querySelector(".typed-text");
+const typedtext = document.getElementById("typed-text");
+const text="Wheels and Deals !!";
+const typingDelay = 200;
+const erasingDelay = 100;
+let charCount = 0;
 
-function typing() {
-    if(i<text.length){
-        document.getElementById("text").innerHTML += text.charAt(i);
-        i++;
-        setTimeout(text,100);
+function type(){
+    if(charCount < text.length){
+        typedtext.innerHTML += text.charAt(charCount); 
+        charCount++;
+        setTimeout(type, typingDelay)
+    }
+    else{
+        setTimeout(erase, erasingDelay);
+       
     }
 }
-typing();
+
+function erase(){
+    if(charCount > 0){
+        typedtext.textContent = text.substring(0,charCount-1);
+        charCount--;
+        setTimeout(erase,erasingDelay);
+    }
+    else{
+        charcount=0;
+        setTimeout(type, typingDelay+1100);
+    }
+}
+type();
