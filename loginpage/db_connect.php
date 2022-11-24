e1dc7a87360c99898ddd2ffb468222b3184bcc39
tejas-srinivas <stejas2002@gmail.com>
2022-11-24 12:15:08 +0530

    UI Changes

--- loginpage/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include ('../loginpage/connect.php');  
-    session_start();
-    if(isset($_SESSION['username'])&&isset($_SESSION['password']))  {  
-        $username = $_SESSION['username'];
-        $password = $_SESSION['password']; 
-    }
-?>
